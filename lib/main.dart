c292ab6c643525e38762a32932644ca8f7b2c770
Ramansharama-worksdelight <reachtoraman@gmail.com>
2021-11-25 23:32:56 +0530

    App completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gender_prediction/Pages/api.dart';
 import 'package:gender_prediction/Pages/homepage.dart';
 
 void main() {
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return 
-     MaterialApp(debugShowCheckedModeBanner: false,
+   const  MaterialApp(debugShowCheckedModeBanner: false,
       home:Homepage(),
       
 
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,14 +2,14 @@ import 'package:flutter/cupertino.dart';
 
 class AppColor{
 
-static Color gradientStartColor = Color(0xFF192741);
-static Color gradientFirstColor = Color(0xFF4A3752);
-static Color gradientSecondColor = Color(0xFF7D4964);
-static  Color gradientThirdColor = Color(0xFFC7617F);
-static Color gradientFourthColor = Color(0xFFFA93A3);
-static Color gradientEndColor = Color(0xFFF8D8C9);
-static Color nameColor = Color(0xFF3F455A);
-static Color ratingCol = Color(0XFFEF544A);
+static const Color gradientStartColor = Color(0xFF192741);
+static const Color gradientFirstColor = Color(0xFF4A3752);
+static const Color gradientSecondColor = Color(0xFF7D4964);
+static  const Color gradientThirdColor = Color(0xFFC7617F);
+static const Color gradientFourthColor = Color(0xFFFA93A3);
+static const Color gradientEndColor = Color(0xFFF8D8C9);
+static const Color nameColor = Color(0xFF3F455A);
+static const Color ratingCol = Color(0XFFEF544A);
 
 
 
